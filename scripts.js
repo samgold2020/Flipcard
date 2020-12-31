7c4f99979983489311b89fd495ba7b84c6fdbade
Samantha Goldstein <samanthagoldstein@Samanthas-MacBook-Pro-2.local>
2020-12-31 17:25:07 -0600

    grid setup, pausing on grid to set up directions modal

--- scripts.js
+++ scripts.js
@@ -5,3 +5,27 @@ function flipCard(){
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard))
+
+
+//Modal Element
+let modal = document.getElementById("directionsModal");
+//Open Modal Button
+let modalBtn = document.getElementById("modalBtn");
+//Close Modal
+let closeBtn = document.getElementsByClassName("closeBtn")[0];
+// ^ since we are getting all closeBtn class names, even though there is only 
+// one we need to specify which one we want in the array. 
+ 
+//Listen for openModal click
+modalBtn.addEventListener('click', openModal);
+
+function openModal (){
+ modal.style.display =  'block';
+}
+
+//Listen for closeModal click
+closeBtn.addEventListener('click', closeModal);
+
+function closeModal() {
+  modal.style.display = 'none';
+}
