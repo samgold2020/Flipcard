7355e6d14ffe110819bfee7630696916915e822f
Samantha Goldstein <samanthagoldstein@Samanthas-MacBook-Pro-2.local>
2021-01-01 12:25:21 -0600

    working on game conditions, javascript/html

--- scripts.js
+++ scripts.js
@@ -1,12 +1,3 @@
-const cards = document.querySelectorAll('.memory-card');
-
-function flipCard(){
-  this.classList.toggle('flip')
-}
-
-cards.forEach(card => card.addEventListener('click', flipCard))
-
-
 //Modal Element
 let modal = document.getElementById("directionsModal");
 //Open Modal Button
@@ -15,17 +6,50 @@ let modalBtn = document.getElementById("modalBtn");
 let closeBtn = document.getElementsByClassName("closeBtn")[0];
 // ^ since we are getting all closeBtn class names, even though there is only 
 // one we need to specify which one we want in the array. 
- 
+
 //Listen for openModal click
 modalBtn.addEventListener('click', openModal);
-
-function openModal (){
- modal.style.display =  'block';
-}
-
 //Listen for closeModal click
 closeBtn.addEventListener('click', closeModal);
+
+function openModal (){
+  modal.style.display =  'block';
+}
 
 function closeModal() {
   modal.style.display = 'none';
 }
+
+//Functionality for the game: 
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let firstCard;
+let secondCard;
+
+  //this is the .memory-card, so we are accessing the classList of the memory card.
+  //After, the  toggle is a boolean for if the classlist is there. We have a 
+  //Flip class in CSS 
+function flipCard(){
+  this.classList.toggle('flip')
+  //this is console logging the memory-card 
+  // console.log(this)
+
+
+//if hasFlippedCard is false 
+if (!hasFlippedCard){
+  //First card flip action
+  hasFlippedCard = true;
+  firstCard = this;
+  //logs the first card flip and sets hasFlippedCard to true
+  // console.logs({hasFlippedCard, firstCard});
+
+ }else {
+   hasFlippedCard = false;
+   secondCard = this;
+  //logs the second card flip and sets hasFlippedCard to true
+  //  console.log({hasFlippedCard, secondCard});
+ }
+}
+  
+cards.forEach(card => card.addEventListener('click', flipCard))
